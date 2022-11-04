dbf669f26de43b3b3240b956956190d2760f72ba
fernandog555 <44325716+fernandog555@users.noreply.github.com>
2022-11-03 22:22:56 -0500

    Update Exercise 5-8.cs

--- Exercises Unit 5/Exercise 5-8.cs
+++ Exercises Unit 5/Exercise 5-8.cs
@@ -1,37 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static System.Console;
-namespace OdddNumbers
-{
 class OddNums
-
 {
-
-  static void
-  Main ()
-
-  {
-
-    WriteLine ("Odd numbers from 1 to 99. Prints one number per line");
-
-    string output = "";
-    for (int n = 1; n <= 99; n++)
-
+   static void Main()
+   {
+     int max = 99;
+     for (int x = 0; x < max; x++)
       {
-
-        if (n % 2 != 0)
-
-          {
-
-            output = output + n.ToString () + Environment.NewLine;
-          }
+        WriteLine(x += 1);
       }
+   }
 
-    WriteLine (output);
-    ReadKey ();
-  }
-}
 }
